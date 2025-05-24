3b90a42056508d1a9c9d8cb297104d3189f84cda
kcoul <kieran.coulter@gmail.com>
2025-05-24 15:33:10 -0700

    Convert to JUCE CLI app to avoid memory leaks

--- NeuralNoteCLI/main.cpp
+++ NeuralNoteCLI/main.cpp
@@ -1,4 +1,5 @@
 #include <juce_core/juce_core.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_audio_formats/juce_audio_formats.h>
 
 #include "BasicPitch.h"
@@ -22,103 +23,118 @@ std::string convertMillis(long long milliseconds) {
                std::to_string(seconds.count());
 }
 
-int main(int argc, const char **argv)
+struct App : juce::JUCEApplication
 {
-    if (argc != 3)
+    const juce::String getApplicationName() override
     {
-        std::cerr << "Usage: " << argv[0] << " <wav file> <out dir>"
-                  << std::endl;
-        exit(1);
+        return "NeuralNoteCLI";
     }
 
-    // load audio passed as argument
-    std::string wav_file = argv[1];
-    juce::File input { wav_file };
-
-    juce::AudioFormatManager formatManager;
-    formatManager.registerBasicFormats();
-
-    std::unique_ptr<juce::FileInputStream> inputStream = input.createInputStream();
-
-    std::unique_ptr<juce::AudioBuffer<float>> buffer = std::make_unique<juce::AudioBuffer<float>>();
-
-    juce::AudioFormatReader* reader = formatManager.createReaderFor (input);
-
-    if (reader != nullptr)
+    const juce::String getApplicationVersion() override { return "0.1"; }
+    void initialise(const juce::String& inputArgs) override
     {
-        buffer->setSize(reader->numChannels, (int)reader->lengthInSamples);
-        reader->read(buffer.get(),
-                     0,
-                     (int)reader->lengthInSamples,
-                     0,
-                     true,
-                     true);
-    }
+        juce::StringArray tokens;
+        tokens.addTokens (inputArgs, " ", "\"");
 
-    // strip extension to make prefix for output filenames
-    std::filesystem::path output_file_prefix = wav_file;
-    output_file_prefix.replace_extension();
-
-    // output dir passed as argument
-    std::string out_dir = argv[2];
-
-    // Check if the output directory exists, and create it if not
-    std::filesystem::path output_dir_path(out_dir);
-    if (!std::filesystem::exists(output_dir_path))
-    {
-        std::cerr << "Directory does not exist: " << out_dir << ". Creating it."
-                  << std::endl;
-        if (!std::filesystem::create_directories(output_dir_path))
+        if (tokens.size() != 2)
         {
-            std::cerr << "Error: Unable to create directory: " << out_dir
+            std::cerr << "Usage: <wav file> <out dir>"
                       << std::endl;
-            return 1;
+            exit(1);
         }
+
+        // load audio passed as argument
+        std::string wav_file = tokens[0].toStdString();
+        juce::File input { wav_file };
+
+        juce::AudioFormatManager formatManager;
+        formatManager.registerBasicFormats();
+
+        std::unique_ptr<juce::FileInputStream> inputStream = input.createInputStream();
+
+        std::unique_ptr<juce::AudioBuffer<float>> buffer = std::make_unique<juce::AudioBuffer<float>>();
+
+        juce::AudioFormatReader* reader = formatManager.createReaderFor (input);
+
+        if (reader != nullptr)
+        {
+            buffer->setSize(reader->numChannels, (int)reader->lengthInSamples);
+            reader->read(buffer.get(),
+                         0,
+                         (int)reader->lengthInSamples,
+                         0,
+                         true,
+                         true);
+        }
+
+        // strip extension to make prefix for output filenames
+        std::filesystem::path output_file_prefix = wav_file;
+        output_file_prefix.replace_extension();
+
+        // output dir passed as argument
+        std::string out_dir = tokens[1].toStdString();
+
+        // Check if the output directory exists, and create it if not
+        std::filesystem::path output_dir_path(out_dir);
+        if (!std::filesystem::exists(output_dir_path))
+        {
+            std::cerr << "Directory does not exist: " << out_dir << ". Creating it."
+                      << std::endl;
+            if (!std::filesystem::create_directories(output_dir_path))
+            {
+                std::cerr << "Error: Unable to create directory: " << out_dir
+                          << std::endl;
+                return;
+            }
+        }
+        else if (!std::filesystem::is_directory(output_dir_path))
+        {
+            std::cerr << "Error: " << out_dir << " exists but is not a directory!"
+                      << std::endl;
+            return;
+        }
+
+        BasicPitch basicPitch;
+
+        float inNoteSensitivity = 0.7;
+        float inPitchSensitivity = 0.5;
+        float inMinNoteDurationMs = 125;
+
+        basicPitch.setParameters(inNoteSensitivity, inPitchSensitivity, inMinNoteDurationMs);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        basicPitch.transcribeToMIDI(buffer->getWritePointer(0),
+                                    buffer->getNumSamples());
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto processingTime = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+        std::cout << "Stems split in " << convertMillis(processingTime.count()) << "s" << std::endl;
+
+        auto noteEvents = basicPitch.getNoteEvents();
+
+        auto outFileName = output_file_prefix.string() + ".mid";
+
+        auto fullOutputPath = output_dir_path / outFileName;
+
+        juce::File outFile { fullOutputPath.string() };
+
+        MidiFileWriter midiFileWriter;
+        TimeQuantizeOptions::TimeQuantizeInfo quantizeInfo;
+        double bpm = 120;
+        PitchBendModes pitchBendMode = PitchBendModes::NoPitchBend;
+
+        if (!midiFileWriter.writeMidiFile(noteEvents, outFile, quantizeInfo, bpm, pitchBendMode)) {
+            std::cout << "Writing MIDI file failed" << std::endl;
+        }
+
+        delete(reader);
+
+        quit();
     }
-    else if (!std::filesystem::is_directory(output_dir_path))
+
+    void shutdown() override
     {
-        std::cerr << "Error: " << out_dir << " exists but is not a directory!"
-                  << std::endl;
-        return 1;
+
     }
+};
 
-    BasicPitch basicPitch;
-
-    float inNoteSensitivity = 0.7;
-    float inPitchSensitivity = 0.5;
-    float inMinNoteDurationMs = 125;
-
-    basicPitch.setParameters(inNoteSensitivity, inPitchSensitivity, inMinNoteDurationMs);
-
-    auto start = std::chrono::high_resolution_clock::now();
-    basicPitch.transcribeToMIDI(buffer->getWritePointer(0),
-                                buffer->getNumSamples());
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto processingTime = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << "Stems split in " << convertMillis(processingTime.count()) << "s" << std::endl;
-
-    auto noteEvents = basicPitch.getNoteEvents();
-
-    auto outFileName = output_file_prefix.string() + ".mid";
-
-    auto fullOutputPath = output_dir_path / outFileName;
-
-    juce::File outFile { fullOutputPath.string() };
-
-    MidiFileWriter midiFileWriter;
-
-    TimeQuantizeOptions::TimeQuantizeInfo quantizeInfo;
-
-    double bpm = 120;
-
-    PitchBendModes pitchBendMode = PitchBendModes::NoPitchBend;
-
-    if (!midiFileWriter.writeMidiFile(noteEvents, outFile, quantizeInfo, bpm, pitchBendMode)) {
-        std::cout << "Writing MIDI file failed" << std::endl;
-    }
-
-    delete(reader);
-
-    exit(0);
-}
-
+START_JUCE_APPLICATION(App)
